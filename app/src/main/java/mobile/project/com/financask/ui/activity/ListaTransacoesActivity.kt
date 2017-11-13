b5be55d9938f30cf96df271ea62c8a5d8c68f14b
Andressa Albuquerque <andressa.albuquerque@clickbus.com>
2017-11-13 13:09:39 -0200

    Implement a simple ArrayAdapter

--- app/src/main/java/mobile/project/com/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/mobile/project/com/financask/ui/activity/ListaTransacoesActivity.kt
@@ -2,6 +2,8 @@ package mobile.project.com.financask.ui.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 import mobile.project.com.financask.R
 
 /**
@@ -13,6 +15,13 @@ class ListaTransacoesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
+
+        val transacoes = listOf("Comida - R$ 20,50",
+                "Economia - R$ 100,00")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, transacoes)
+
+        lista_transacoes_listview.adapter = adapter
     }
 
 }
