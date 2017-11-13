1187fbb0f99dd95a3f68bf84532b934dbd417987
Andressa Albuquerque <andressa.albuquerque@clickbus.com>
2017-11-12 22:44:21 -0200

    Create first activity e import all the layouts

---
+++ app/src/main/java/mobile/project/com/financask/ui/activity/ListaTransacoesActivity.kt
@@ -0,0 +1,18 @@
+package mobile.project.com.financask.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import mobile.project.com.financask.R
+
+/**
+ * Created by andressa on 12/11/17.
+ */
+
+class ListaTransacoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+    }
+
+}
