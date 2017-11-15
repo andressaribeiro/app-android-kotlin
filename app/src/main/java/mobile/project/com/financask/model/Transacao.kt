c1d5f5146271c61ceca3ce85669bbfedad3e6e2c
Andressa Albuquerque <andressa.albuquerque@clickbus.com>
2017-11-15 17:52:24 -0200

    Using properties on Transacao model

--- app/src/main/java/mobile/project/com/financask/model/Transacao.kt
+++ app/src/main/java/mobile/project/com/financask/model/Transacao.kt
@@ -6,25 +6,6 @@ import java.util.Calendar
 /**
  * Created by andressa on 14/11/17.
  */
-class Transacao(valor: BigDecimal,
-                categoria: String,
-                data: Calendar) {
-
-    private val valor = valor
-    private val categoria = categoria
-    private val data = data
-
-
-    fun getValor(): BigDecimal {
-        return valor
-    }
-
-    fun getCategoria(): String {
-        return categoria
-    }
-
-    fun getCalendar(): Calendar {
-        return data
-    }
-
-}
+class Transacao(val valor: BigDecimal,
+                val categoria: String,
+                val data: Calendar)
--- app/src/main/java/mobile/project/com/financask/ui/activity/ListaTransacoesAdapter.kt
+++ app/src/main/java/mobile/project/com/financask/ui/activity/ListaTransacoesAdapter.kt
@@ -8,6 +8,7 @@ import android.widget.BaseAdapter
 import kotlinx.android.synthetic.main.transacao_item.view.*
 import mobile.project.com.financask.R
 import mobile.project.com.financask.model.Transacao
+import java.text.SimpleDateFormat
 
 /**
  * Created by andressa on 14/11/17.
@@ -21,7 +22,9 @@ class ListaTransacoesAdapter(transacoes: List<Transacao>,
     override fun getView(posicao: Int, view: View?, parent: ViewGroup?): View {
         val view = LayoutInflater.from(context).inflate(R.layout.transacao_item, parent, false)
         val transacao = transacoes[posicao]
-        view.transacao_valor.setText(transacao.getValor().toString())
+        view.transacao_valor.text = transacao.valor.toString()
+        view.transacao_categoria.text = transacao.categoria
+        view.transacao_data.text = SimpleDateFormat("dd/MM/yyyy").format(transacao.data.time)
         return view
     }
 
